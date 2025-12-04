394939f832593c7fe7d7c11912fe2d85540805b9
Sachiththa Bandaranayake <sacume3@gmail.com>
2025-12-04 02:22:40 +0100

    Create TaskEntit

---
+++ src/main/java/com/dsti/devops_task_manager/TaskStatus.java
@@ -0,0 +1,6 @@
+package com.dsti.devops_task_manager;
+
+public enum TaskStatus {
+    IN_PROGRESS,
+    COMPLETED,
+}
---
+++ src/main/java/com/dsti/devops_task_manager/entities/TaskEntity.java
@@ -0,0 +1,29 @@
+package com.dsti.devops_task_manager.entities;
+import com.dsti.devops_task_manager.TaskStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "task")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+}
