dd568410474ae49d911861c28bc82c8cd68ebc70
Sachiththa Bandaranayake <sacume3@gmail.com>
2025-12-04 04:31:43 +0100

    Add log info

--- src/main/java/com/dsti/devops_task_manager/services/TaskService.java
+++ src/main/java/com/dsti/devops_task_manager/services/TaskService.java
@@ -5,11 +5,13 @@ import com.dsti.devops_task_manager.entities.TaskEntity;
 import com.dsti.devops_task_manager.models.Task;
 import com.dsti.devops_task_manager.repositories.TaskRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.jspecify.annotations.NonNull;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class TaskService {
@@ -18,11 +20,14 @@ public class TaskService {
 
 
     public TaskEntity toTaskEntity(Task task) {
+        if (task == null) {
+            log.warn("toTaskEntity() called with null task");
+            return null;
+        }
 
-        if (task == null) return null;
+        log.debug("Converting Task to TaskEntity: {}", task);
 
-        return TaskEntity.
-                builder()
+        return TaskEntity.builder()
                 .id(task.getId())
                 .title(task.getTitle())
                 .description(task.getDescription())
@@ -30,13 +35,15 @@ public class TaskService {
                 .build();
     }
 
-
     public Task toTask(TaskEntity taskEntity) {
+        if (taskEntity == null) {
+            log.warn("toTask() called with null taskEntity");
+            return null;
+        }
 
-        if (taskEntity == null) return null;
+        log.debug("Converting TaskEntity to Task: {}", taskEntity);
 
-        return Task
-                .builder()
+        return Task.builder()
                 .id(taskEntity.getId())
                 .title(taskEntity.getTitle())
                 .description(taskEntity.getDescription())
@@ -45,35 +52,55 @@ public class TaskService {
     }
 
     public Task createTask(@NonNull Task task) {
-        TaskEntity taskEntity = toTaskEntity(task);
+        log.info("Creating task with title: {}", task.getTitle());
 
+        TaskEntity taskEntity = toTaskEntity(task);
         TaskEntity savedEntity = this.taskRepository.save(taskEntity);
+
+        log.debug("Task created with ID: {}", savedEntity.getId());
 
         return toTask(savedEntity);
     }
 
-
     public Task getTaskById(Long id) {
+        log.info("Fetching task by ID: {}", id);
+
         TaskEntity taskEntity = this.taskRepository.findById(id).orElse(null);
+
+        if (taskEntity == null) {
+            log.warn("Task not found with ID: {}", id);
+        } else {
+            log.debug("Task retrieved: {}", taskEntity);
+        }
+
         return toTask(taskEntity);
     }
 
-
     public void deleteTask(@NonNull Task task) {
+        log.info("Deleting task with ID: {}", task.getId());
+
         this.taskRepository.deleteById(task.getId());
+
+        log.debug("Task deleted: {}", task);
     }
 
-
     public Task updateTask(@NonNull Task task) {
-        TaskEntity taskEntity = toTaskEntity(task);
+        log.info("Updating task with ID: {}", task.getId());
 
+        TaskEntity taskEntity = toTaskEntity(task);
         TaskEntity updatedEntity = this.taskRepository.save(taskEntity);
+
+        log.debug("Task updated: {}", updatedEntity);
 
         return toTask(updatedEntity);
     }
 
     public List<Task> getAllTasks() {
+        log.info("Fetching all tasks");
+
         List<TaskEntity> taskEntities = this.taskRepository.findAll();
+
+        log.debug("Total tasks retrieved: {}", taskEntities.size());
 
         return taskEntities.stream()
                 .map(this::toTask)
