704cca145826d8346fc55fe85a6ab94ddebc6d90
Sachiththa Bandaranayake <sacume3@gmail.com>
2025-12-04 18:45:44 +0100

    Remove createTask exception handling

--- src/main/java/com/dsti/devops_task_manager/services/TaskService.java
+++ src/main/java/com/dsti/devops_task_manager/services/TaskService.java
@@ -3,7 +3,6 @@ package com.dsti.devops_task_manager.services;
 
 import com.dsti.devops_task_manager.dtos.TaskDto;
 import com.dsti.devops_task_manager.entities.TaskEntity;
-import com.dsti.devops_task_manager.exceptions.TaskCreationException;
 import com.dsti.devops_task_manager.models.Task;
 import com.dsti.devops_task_manager.repositories.TaskRepository;
 import lombok.AllArgsConstructor;
@@ -57,15 +56,11 @@ public class TaskService {
         log.info("Creating task with title: {}", task.getTitle());
 
         TaskEntity taskEntity = toTaskEntity(task);
-        try {
-            TaskEntity savedEntity = this.taskRepository.save(taskEntity);
-            log.debug("Task created with ID: {}", savedEntity.getId());
+        TaskEntity savedEntity = this.taskRepository.save(taskEntity);
 
-            return toTask(savedEntity);
-        } catch (Exception ex) {
-            log.error(ex.getMessage(), ex);
-            throw new TaskCreationException(ex.getMessage(), ex);
-        }
+        log.debug("Task created with ID: {}", savedEntity.getId());
+
+        return toTask(savedEntity);
     }
 
 
