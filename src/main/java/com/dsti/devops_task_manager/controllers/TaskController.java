92fc238a3812243942e3dd2dcb0d5a21d3d3a0bc
Sachiththa Bandaranayake <sacume3@gmail.com>
2025-12-04 18:59:44 +0100

    Add health check endpoint api/tasks

--- src/main/java/com/dsti/devops_task_manager/controllers/TaskController.java
+++ src/main/java/com/dsti/devops_task_manager/controllers/TaskController.java
@@ -1,22 +1,23 @@
 package com.dsti.devops_task_manager.controllers;
 
-
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.jspecify.annotations.NonNull;
 import org.springframework.boot.health.contributor.Health;
 import org.springframework.boot.health.contributor.Status;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/api/tasks")
-public class UserController {
+@AllArgsConstructor
+@Slf4j
+public class TaskController {
 
     @GetMapping("/health")
     public ResponseEntity<@NonNull Health> healthCheck() {
         return ResponseEntity.ok(Health.status(Status.UP).build());
     }
-
 
 }
